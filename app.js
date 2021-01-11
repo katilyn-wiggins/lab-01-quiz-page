58977a4c713007e9eaa7a01533cf2a728be19c83
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-01-11 17:41:34 -0500

    confirmation message

--- app.js
+++ app.js
@@ -2,5 +2,17 @@
 const quizButton = document.getElementById('quiz-button');
 const answerDiv = document.getElementById('hidden-div');
 // initialize state
+quizButton.addEventListener('click', () => {
+    alert('Would you like to take a quiz about raccoons?');
+    const takeQuiz = confirm('Are you suuuuure?');
+    if (takeQuiz === false) {
+        return;
+    }
 
+
+
+
+
+
+});
 // set event listeners to update state and DOM
